265d0f1e6e3cf9629c038579235c740f10f9f003
Marius Rundereim <marius.rundereim@gmail.com>
2024-03-25 19:28:04 +0100

    search added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useStore } from "../../services/store/store";
 import ProductCard from "../../components/Cards/ProductCard";
 import SearchBar from "../../components/SearchBar";
@@ -6,16 +6,29 @@ import { Grid } from "@mui/material";
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Stack } from "@mui/material";
+
 function Home() {
   const { products, fetchProducts } = useStore();
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
-    fetchProducts().catch(console.error);
+    fetchProducts();
   }, []);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const searchResults = products.filter(
+    (product) =>
+      product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      product.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <>
       <Box component="main">
-        <SearchBar />
+        <SearchBar value={searchQuery} onChange={handleSearchChange} />
 
         <Stack direction="row" justifyContent="space-between">
           <Typography variant="h4">Products</Typography>
@@ -23,7 +36,7 @@ function Home() {
         </Stack>
 
         <Grid container spacing={4}>
-          {products.map((product) => (
+          {searchResults.map((product) => (
             <Grid item sm={4} key={product.id}>
               <ProductCard product={product} />
             </Grid>
