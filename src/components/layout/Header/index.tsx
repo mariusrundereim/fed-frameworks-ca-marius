894a106a75be46bcfae42a3894f28545a6ddc750
Marius Rundereim <marius.rundereim@gmail.com>
2024-04-01 13:47:50 +0200

    logo fix

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,10 +1,10 @@
+import { Link } from "react-router-dom";
 import { useStore } from "../../../services/store/store";
 import { AppBar, Box, Typography } from "@mui/material";
 import Badge from "@mui/material/Badge";
 import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import { Stack } from "@mui/material";
 import BrandLogo from "../../Logo";
-import { Link } from "react-router-dom";
 function Header() {
   const cartItemCount = useStore((state) =>
     state.cart.reduce((total, item) => total + item.quantity, 0)
@@ -20,24 +20,23 @@ function Header() {
           <Stack
             display="flex"
             justifyItems="center"
-            direction="row"
+            direction={{ xs: "column", sm: "row" }}
             justifyContent="space-between"
             p="1rem"
             width="100%"
-            margin="0 auto"
             sx={{ maxWidth: "1440px" }}
           >
             <BrandLogo />
             <Link to="/cart">
               <Stack spacing={1} direction="row">
-                <Typography>Cart</Typography>
+                <Typography variant="body1">Cart</Typography>
                 <Badge
                   color="secondary"
                   badgeContent={cartItemCount}
                   max={999}
                   showZero
                 >
-                  <ShoppingBagIcon color="action" />
+                  <ShoppingBagIcon color="action" fontSize="large" />
                 </Badge>
               </Stack>
             </Link>
