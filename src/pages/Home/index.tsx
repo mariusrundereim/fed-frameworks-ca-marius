cf0e2710791d0f4beb787d0167882b04cad8be26
Marius Rundereim <marius.rundereim@gmail.com>
2024-04-03 10:03:29 +0200

    separate search from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useStore } from "../../services/store/store";
 import ProductCard from "../../components/Cards/ProductCard";
-import SearchBar from "../../components/SearchBar";
 import { Grid } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Stack } from "@mui/material";
@@ -9,55 +8,37 @@ import SearchBarNew from "../../components/Search";
 
 function Home() {
   const { products, fetchProducts } = useStore();
-  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
     fetchProducts();
   }, []);
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const searchResults = products.filter(
-    (product) =>
-      product.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      product.description.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
   return (
     <>
       <SearchBarNew placeholder="Search.." data={products} />
-      <SearchBar value={searchQuery} onChange={handleSearchChange} />
 
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="h4">Products</Typography>
-        <Typography variant="h4">{searchResults.length} items</Typography>
+        <Typography variant="h4">{products.length} items</Typography>
       </Stack>
 
-      {searchResults.length > 0 ? (
-        <Grid container component="section" spacing={{ xs: 1, sm: 3, md: 4 }}>
-          {searchResults.map((product) => (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              lg={3}
-              key={product.id}
-              sx={{
-                minWidth: "100px",
-                maxWidth: "1",
-              }}
-            >
-              <ProductCard product={product} />
-            </Grid>
-          ))}
-        </Grid>
-      ) : (
-        <Typography variant="h6" style={{ marginTop: 20 }}>
-          No products found
-        </Typography>
-      )}
+      <Grid container spacing={3}>
+        {products.map((product) => (
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            lg={3}
+            key={product.id}
+            sx={{
+              minWidth: "100px",
+              maxWidth: "1",
+            }}
+          >
+            <ProductCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
